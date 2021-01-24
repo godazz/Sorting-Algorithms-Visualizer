6411b376433fa77884e3281e081742ed3811790f
Ahmed Abd El-Gawad <ahmedabdelgowadramadan@gmail.com>
2021-01-24 20:25:04 +0200

    Created functions to generate random array

--- app.js
+++ app.js
@@ -1,7 +1,46 @@
-var slider = document.getElementById("range");
-var output = document.getElementById("array-size");
-output.innerHTML = slider.value;    
+var Arr = [];
+var len = Arr.length;
 
-slider.oninput = function() {
-    output.innerHTML = this.value;
+
+
+generateArray(50);
+randomArray();
+printArray(Arr);
+
+
+
+  function generateArray(size){
+    Arr = [];
+    for (var i = 1 ; i <= size ; i++){
+      Arr.push(i);
+    }
+    len = Arr.length;
+    return Arr;
   }
+
+  function randomArray(){
+      var index =len , random;
+    
+        while(0!== index){
+            random = Math.floor(Math.random() * index);
+            index -= 1;
+
+            swap(Arr,index,random);
+        }
+        // draw the array;
+        return Arr;
+  }
+
+  function swap (arr , i , j ){
+      var temp = arr[i];
+      arr[i] = arr[j];
+      arr[j] = temp;
+      return arr;
+  }
+
+    function printArray (arr){
+      for (var i = 0 ;i < arr.length ; i++){
+        console.log(arr[i]);
+      }
+    }
+
