d2cc7ccec5bdd527ef9191acb85131a87beceb4f
Ahmed Abd El-Gawad <ahmedabdelgowadramadan@gmail.com>
2021-01-31 09:44:57 +0200

    Animated Selection and bubble Sort

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-var Arr = [];
-var len = Arr.length;
+let Arr = [];
+let len = Arr.length;
 
-var canvas = document.getElementById('canvas');
-var ctx = canvas.getContext("2d");
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext("2d");
 
-var range = document.getElementById("range");
-var showlen = document.getElementById("array-size");
+const range = document.getElementById("range");
+const showlen = document.getElementById("array-size");
 
 showlen.innerHTML = range.value;
 
@@ -17,7 +17,7 @@ drawArray(Arr);
 
   function generateArray(size){
     Arr = [];
-    for (var i = 1 ; i <= size ; i++){
+    for (let i = 1 ; i <= size ; i++){
       Arr.push(i);
     }
     len = Arr.length;
@@ -25,7 +25,7 @@ drawArray(Arr);
   }
 
   function randomArray(){
-      var index =len , random;
+      let index =len , random;
     
         while(index !== 0){
             random = Math.floor(Math.random() * index);
@@ -38,55 +38,74 @@ drawArray(Arr);
   }
 
   function swap (arr, i , j){
-      var temp = arr[i];
+      let temp = arr[i];
       arr[i] = arr[j];
       arr[j] = temp;
       return arr;
   }
 
-  function drawArray(arr){
-    var x = 0;
-    var wid = canvas.width/ len ;
+  function drawArray(arr, idx){
+    let x = 0;
+    const wid = canvas.width/ len ;
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-      for (var i = 0 ; i < len ; i++){
+      for (let i = 0 ; i < len ; i++){
         ctx.beginPath();
-        ctx.fillStyle = "#424242";
+        if(i===idx ){
+          ctx.fillStyle = "#21209c";
+        }else{
+          ctx.fillStyle = "#424242";
+        }
         ctx.fillRect (x, (canvas.height) - arr[i] * (wid/3), Math.floor(wid) -2 , (canvas.height));
         ctx.closePath();
         x+= wid;
       }
   }
-
+ 
   function worstCase(){
-    var tmp = len;
-    for (var i = 0 ; i < len ; i++){
+    let tmp = len;
+    for (let i = 0 ; i < len ; i++){
       Arr[i] = tmp--;
     }
     drawArray(Arr);
   }
 
-  function selectionSort (){
-    for (var i = 0 ; i < len ; i++){
-        var min = i;
-        for (var j = i+1 ; j < len ;  j++){
-          if (Arr[j] < Arr[min])
-            min = j;
-        }
-        swap(Arr, min , i);
-         drawArray(Arr);
-    }
-    
+  function sleep(ms){
+		return new Promise(resolve =>setTimeout(resolve,ms));	
   }
+  
 
-  function bubbleSort (){
-    for (var i = 0 ; i < len ; i++){
-      for (var j = 0 ; j < len - i - 1 ; j++){
-        if (Arr[j] > Arr[j+1])
-            swap(Arr, j, j+1);
+    async function selectionSort (){
+    for (let i = 0 ; i < len ; i++){
+        let min = i;
+       await drawArray(Arr , min);
+        for (let j = i+1 ; j < len ;  j++){
+          if (Arr[j] < Arr[min]){
+            min = j;
+           await sleep(500);
+           await drawArray(Arr , min);
+        }
       }
+       await swap(Arr, min , i);
+       await drawArray(Arr,min);
+       await sleep(500);
     }
     drawArray(Arr);
   }
+
+   async function bubbleSort (){
+    for (let i = 0 ; i < len ; i++){
+      for (let j = 0 ; j < len - i - 1 ; j++){
+         await drawArray(Arr , j);  
+        if (Arr[j] > Arr[j+1]){         
+            swap(Arr, j, j+1);
+           await drawArray(Arr , j);
+           await sleep(200);
+      }
+    }
+  }
+  drawArray(Arr);
+}
+
 
 range.oninput = function(){
   generateArray(range.value);
